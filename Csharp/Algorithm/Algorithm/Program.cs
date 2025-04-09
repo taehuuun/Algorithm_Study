a549f08ad0b8f9be3079faef3e28ae8a678ffe14
LimTaeHun <43982651+taehuuun@users.noreply.github.com>
2025-04-09 12:07:35 +0900

    Feat: 간단한 프레임 계산 로직 구현

--- Csharp/Algorithm/Algorithm/Program.cs
+++ Csharp/Algorithm/Algorithm/Program.cs
@@ -2,8 +2,28 @@
 
 class Program
 {
+    private const int _WAIT_TIME = 1000 / 30;
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+        int lastTick = 0;
+        
+        while (true)
+        {
+            #region 프레임
+
+            int currentTick = Environment.TickCount;
+            
+            // 만약 경과한 시간이 1/30초 보다 작다면 continue
+            if (currentTick - lastTick < _WAIT_TIME)
+            {
+                continue;
+            }
+            
+            lastTick = currentTick;
+
+            #endregion
+        }
     }
 }
